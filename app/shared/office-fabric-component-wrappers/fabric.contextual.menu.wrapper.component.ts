45258c73bcaa1a6cc0682d959b3f31280e4e2274
Ricky Kirkham <rickki@microsoft.com>
2016-09-15 14:42:43 -0700

    change OAIs to OAI because the search deals only with whole words

--- app/shared/office-fabric-component-wrappers/fabric.contextual.menu.wrapper.component.ts
+++ app/shared/office-fabric-component-wrappers/fabric.contextual.menu.wrapper.component.ts
@@ -29,7 +29,7 @@ export class FabricContextualMenuWrapperComponent implements AfterViewInit {
         this.wordDocument.replaceDocumentContent([
             "Office Add-ins",
             "An OAI runs in an Office application and can interact with data in a document or mail item. As one observer said:",
-            "\t\"These new OAIs are the cat's meow.\"",
+            "\t\"The new OAI model is the cat's meow.\"",
             "An OAI is a web app that you can host anywhere. It runs in an Office application. A manifest.xml file specifies where the web app is located and how it should appear.",
             "You can find an OAI sample or two (or many) in the OfficeDev organization on GitHub."
         ]);
